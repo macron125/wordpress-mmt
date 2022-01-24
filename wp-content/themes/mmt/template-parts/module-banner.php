e01233f890ee5bcd4d501a099b8e0dd0228bed26
George Paresishvili <g.paresishvili@outlook.com>
2022-01-24 16:24:10 +0400

    Remove header for 'book a visit' page

--- wp-content/themes/mmt/template-parts/module-banner.php
+++ wp-content/themes/mmt/template-parts/module-banner.php
@@ -1,14 +1,16 @@
 <section class="module module-hero mmt-hero-banner">
-    <div class="mmt-hero-banner-img-container">
-      <div class="mmt-hero-banner-overlay"></div>
-      <img class="mmt-hero-banner-img" src="<?php echo esc_url( get_post_custom_values('hero_image')[0] ); ?>" alt="">  
-    </div>
-    <div class="mmt-hero-headline-container">
-      <h1 class="mmt-hero-banner-headline"><?php the_title(); ?></h1>
-    </div>
-  </section>
-  <nav class="mmt-page-nav">
-    <ul class="mmt-page-nav-list">
-      <?php // Menu goes here main.js ?>
-    </ul>
-  </nav>
+  <div class="mmt-hero-banner-img-container">
+    <div class="mmt-hero-banner-overlay"></div>
+    <img class="mmt-hero-banner-img" src="<?php echo esc_url( get_post_custom_values('hero_image')[0] ); ?>" alt="">
+  </div>
+  <div class="mmt-hero-headline-container">
+    <h1 class="mmt-hero-banner-headline"><?php the_title(); ?></h1>
+  </div>
+</section>
+<?php if( !is_page( 'book-a-visit' ) ) :?>
+<nav class="mmt-page-nav">
+  <ul class="mmt-page-nav-list">
+    <?php // Menu goes here main.js ?>
+  </ul>
+</nav>
+<?php endif; ?>
