96d83eb32838d6751a8ae73a457bcb5aadbe1cb3
George Paresishvili <g.paresishvili@outlook.com>
2022-01-24 21:03:26 +0400

    Add contact for style

--- wp-content/themes/mmt/template-parts/module-contact_form.php
+++ wp-content/themes/mmt/template-parts/module-contact_form.php
@@ -1,36 +1,47 @@
 <section class="module module-contact_us mmt-contact_us">
+
   <form action="" class="mmt-contact_us-form">
+
+    <fieldset class="mmt-contact_us-fieldset">
+
+      <div>
+        <label for="firstname">First Name</label>
+        <input class="mmt-field-short" type="text" name="firstname" id="firstname" placeholder="First Name" required>
+      </div>
+
+      <div>
+        <label for="lastname">Last Name</label>
+        <input class="mmt-field-short" type="text" name="lastname" id="lastname" placeholder="Last Name" required>
+      </div>
+    
+    </fieldset>
+
     <div>
-      <label class="visually-hidden screen-reader-text" for="name">Name</label>
-      <input type="text" placeholder="Name" id="name" name="name">
+      <label for="phone">Phone</label>
+      <input class="mmt-field-long" type="text" name="phone" id="phone" placeholder="Phone" required>
     </div>
+    
     <div>
-      <label class="visually-hidden screen-reader-text" for="email">Email</label>
-      <input type="text" placeholder="Email" id="email" name="email">
+      <label for="email">E-mail</label>
+      <input class="mmt-field-long" type="text" name="email" id="email" placeholder="E-mail">
     </div>
+    
     <div>
-      <label class="visually-hidden screen-reader-text" for="Phone">Phone</label>
-      <input type="text" placeholder="Phone" id="phone" name="phone">
+      <label for="message">Message</label>
+      <textarea class="mmt-field-long" name="message" id="message" cols="30" rows="10" placeholder="Message" required></textarea>
     </div>
-    <div>
-      <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" name="message"></textarea>
-    </div>
+
     <button type="submit">Submit</button>
+
   </form>
+
   <div class="mmt-contact_us-info">
-    <address class="mmt-contact_us">
-      <a class="phone" href="tel:+995599999999">+995 599 99 99 99</a>
-      <a class="email" href="mailto:mmt@mmthospital.com">mmt@mmthospital.com</a>
-      <span class="address">MMT Hospital <br>
-        5 Lubliana Str, <br>
-        Tbilisi, Georgia
-      </span>
-    </address>
     <iframe 
+    class="gmap"
     loading="lazy" 
     style="border: 0;" 
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12610.072118972997!2d44.77192509505746!3d41.77269183336443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40446d9318133e1f%3A0x21a64789cd26637a!2sMMT+Hospital!5e0!3m2!1sen!2sge!4v1524458741660" 
-    width="100%" 
+    width="80%" 
     height="450" 
     frameborder="0" 
     allowfullscreen="allowfullscreen" 
