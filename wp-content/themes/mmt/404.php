9fa702e1220a01707714b220b222408c5a38e8ff
George Paresishvili <g.paresishvili@outlook.com>
2022-01-26 20:36:58 +0400

    Added content to 404 error page

--- wp-content/themes/mmt/404.php
+++ wp-content/themes/mmt/404.php
@@ -2,9 +2,11 @@
 
 get_header();
 
-echo '<main>';
+echo '<main style="padding:100px 0;">';
 
-  
+  echo '<h1 style="text-align:center;font-size:5rem;">404</h1>';
+
+  echo '<h2 style="text-align:center;">We are sorry, the page you requested cannot be found.</h2>';
 
 echo '</main>';
 get_footer();
