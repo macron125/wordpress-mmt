4e4416ea3a54cb589edac5174f8b3ec3b4fe5c16
George Paresishvili <g.paresishvili@outlook.com>
2022-01-26 20:56:30 +0400

    Added text to 404.php

--- wp-content/themes/mmt/404.php
+++ wp-content/themes/mmt/404.php
@@ -4,9 +4,11 @@ get_header();
 
 echo '<main style="padding:100px 0;">';
 
-  echo '<h1 style="text-align:center;font-size:5rem;">404</h1>';
+  echo '<h1 style="text-align:center;font-size:5rem;">404.</h1>';
 
   echo '<h2 style="text-align:center;">We are sorry, the page you requested cannot be found.</h2>';
+
+  echo "<p style='text-align:center;'>The URL may be misspelled or the page you're looking for is no longer available.</p>";
 
 echo '</main>';
 get_footer();
