1118afd6a6f8eb745a3d16d0ce0f293530bf72c1
George Paresishvili <g.paresishvili@outlook.com>
2022-01-27 14:30:00 +0400

    Add elements of the blog module structure (styles added previously)

--- wp-content/themes/mmt/template-parts/modules/module-blog.php
+++ wp-content/themes/mmt/template-parts/modules/module-blog.php
@@ -1,3 +1,12 @@
 <section class="module module-blog">
-  <?php the_content(); ?>
+  <article class="mmt-article">
+    <ul class="mmt-article-data">
+      <li class="mmt-article-data-category"><?php the_category(); ?></li>
+      <li class="mmt-article-data-date"><?php echo get_the_date(); ?></li>
+    </ul>
+
+    <div class="mmt-article-content">
+      <?php the_content(); ?>
+    </div>
+  </article>
 </section>
