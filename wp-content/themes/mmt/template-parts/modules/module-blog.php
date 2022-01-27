27444899318b7eea722deade5233b2982dfd9feb
George Paresishvili <g.paresishvili@outlook.com>
2022-01-27 15:01:00 +0400

    Add posts recognition for the blog module added because date was indicated on who we are and what we do page

--- wp-content/themes/mmt/template-parts/modules/module-blog.php
+++ wp-content/themes/mmt/template-parts/modules/module-blog.php
@@ -1,9 +1,12 @@
 <section class="module module-blog">
   <article class="mmt-article">
+    <?php // Check if its a single post. If its page, omit the data ribbon. ?>
+    <?php if( is_single() ) : ?>
     <ul class="mmt-article-data">
       <li class="mmt-article-data-category"><?php the_category(); ?></li>
       <li class="mmt-article-data-date"><?php echo get_the_date(); ?></li>
     </ul>
+    <?php endif; ?>
 
     <div class="mmt-article-content">
       <?php the_content(); ?>
