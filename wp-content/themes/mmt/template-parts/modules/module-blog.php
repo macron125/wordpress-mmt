2cea5b56979947aae7c650b5dfad0c0f4d7b0d74
George Paresishvili <g.paresishvili@outlook.com>
2022-01-27 17:12:07 +0400

    Add new card conditional styles

--- wp-content/themes/mmt/template-parts/modules/module-blog.php
+++ wp-content/themes/mmt/template-parts/modules/module-blog.php
@@ -1,10 +1,10 @@
 <section class="module module-blog">
-  <article class="mmt-article">
+  <article class="mmt-article" id="aid-<?php the_ID(); ?>">
     <?php // Check if its a single post. If its page, omit the data ribbon. ?>
     <?php if( is_single() ) : ?>
     <ul class="mmt-article-data">
-      <li class="mmt-article-data-category"><?php the_category(); ?></li>
       <li class="mmt-article-data-date"><?php echo get_the_date(); ?></li>
+      <li class="mmt-article-data-category ribbon"><?php the_category(); ?></li>
     </ul>
     <?php endif; ?>
 
