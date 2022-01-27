47a893c37728e316ea1579b4b2450c8db453c519
George Paresishvili <g.paresishvili@outlook.com>
2022-01-27 14:28:14 +0400

    Change the way banner module retrieves the titles

--- wp-content/themes/mmt/template-parts/modules/module-banner.php
+++ wp-content/themes/mmt/template-parts/modules/module-banner.php
@@ -2,10 +2,10 @@
     <section class="module module-hero mmt-hero-banner">
       <div class="mmt-hero-banner-img-container">
         <div class="mmt-hero-banner-overlay"></div>
-        <img class="mmt-hero-banner-img" src="<?php echo esc_url( get_post_custom_values('hero_image')[0] ); ?>" alt="">
+        <img class="mmt-hero-banner-img" src="<?php echo esc_url( get_post_custom_values( 'hero_image' )[0] ); ?>" alt="">
       </div>
       <div class="mmt-hero-headline-container">
-        <h1 class="mmt-hero-banner-headline"><?php the_title(); ?></h1>
+        <h1 class="mmt-hero-banner-headline"><?php echo single_post_title(); ?></h1>
       </div>
     </section>
   <?php elseif(get_post_custom_values('hero_video')) : ?>
@@ -18,11 +18,11 @@
   <?php else : ?>
     <section class="module module-hero mmt-hero-banner">
       <div class="mmt-hero-headline-container nobanner">
-        <h1 class="mmt-hero-banner-headline"><?php the_title(); ?></h1>
+        <h1 class="mmt-hero-banner-headline"><?php echo single_post_title(); ?></h1>
       </div>
     </section>
   <?php endif; ?>
-<?php if( !is_page( [ 'book-a-visit', 'privacy-policy', ] ) && !is_front_page() && !is_single() ) :?>
+<?php if( !is_page( [ 'book-a-visit', 'privacy-policy', ] ) && !is_front_page() && !is_single() && !is_home() ) :?>
   <nav class="mmt-page-nav">
     <ul class="mmt-page-nav-list">
       <?php // Menu goes here main.js ?>
